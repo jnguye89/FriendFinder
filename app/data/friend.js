03f232682197cffe503c84a766defe0ee49546f6
jnguye89 <Jackie@Jackies-MacBook-Pro.local>
2017-12-23 16:15:16 -0600

    working app

---
+++ app/data/friend.js
@@ -0,0 +1,21 @@
+var friendArray = [
+    {
+        friendName: "Jackie", 
+        imageUrl: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.blog.google%2Fstatic%2Fblog%2Fimages%2Fgoogle-200x200.7714256da16f.png&imgrefurl=https%3A%2F%2Fwww.blog.google%2F&docid=qNYvlP1f5mOdfM&tbnid=rG6yjYarVr_P9M%3A&vet=10ahUKEwjFg_jorZzYAhUHyoMKHZiTB1sQMwjCASgBMAE..i&w=200&h=200&bih=755&biw=1398&q=google&ved=0ahUKEwjFg_jorZzYAhUHyoMKHZiTB1sQMwjCASgBMAE&iact=mrc&uact=8",
+        scores: [
+            "1", 
+            "3", 
+            "5", 
+            "2", 
+            "3",
+            "4", 
+            "2", 
+            "3",
+            "5",
+            "1"
+        ] 
+    }
+]
+
+
+module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,40 @@
-var path = require("path");
+var friendArray = require('../data/friend');
 
-
-module.exports = function(app) {
-
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
+module.exports = function(app){
+  app.get("/api/friends", function(req, res){
+    res.json(friendArray);
   });
 
-  app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
 
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+  app.post("/api/friends", function(req, res){
+    friendArray.push(req.body);
+    
+    //variable will hold the current best match friend in the friendArray
+    var bestMatch;
+    //variable to hold the current difference between best match friend
+    var bestMatchTotal = 1000;
+
+    //loop to see which friend matches
+    for (var i = 0; i < friendArray.length-1; i ++){
+      var q1 = Math.abs(friendArray[i].scores[0] - req.body.scores[0]);
+      var q2 = Math.abs(friendArray[i].scores[1] - req.body.scores[1]);
+      var q3 = Math.abs(friendArray[i].scores[2] - req.body.scores[2]);
+      var q4 = Math.abs(friendArray[i].scores[3] - req.body.scores[3]);
+      var q5 = Math.abs(friendArray[i].scores[4] - req.body.scores[4]);
+      var q6 = Math.abs(friendArray[i].scores[5] - req.body.scores[5]);
+      var q7 = Math.abs(friendArray[i].scores[6] - req.body.scores[6]);
+      var q8 = Math.abs(friendArray[i].scores[7] - req.body.scores[7]);
+      var q9 = Math.abs(friendArray[i].scores[8] - req.body.scores[8]);
+      var q10 = Math.abs(friendArray[i].scores[9] - req.body.scores[9]);
+      var total = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10;
+      console.log(total);
+      if (total < bestMatchTotal){
+        bestMatchTotal = total;
+        bestMatch = friendArray[i];
+      }
+      
+    }
+
+    res.json(bestMatch);
   });
-};
+}
